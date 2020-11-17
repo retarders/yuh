e329c1bf7de6ded0ee8d88021ce33f5e1769551d
pcranaway <pcranaway@tuta.io>
2020-11-17 09:10:59 +0000

    add help command

--- cli/src/main/java/top/retarders/yuh/command/HelpCommand.java
+++ cli/src/main/java/top/retarders/yuh/command/HelpCommand.java
@@ -1,6 +1,11 @@
 package top.retarders.yuh.command;
 
 import io.airlift.airline.Command;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.HashMap;
+
+import static org.apache.commons.lang.StringUtils.*;
 
 @Command(
         name = "help",
@@ -8,7 +13,19 @@ import io.airlift.airline.Command;
 )
 public class HelpCommand implements Runnable {
 
+    /**
+     * Defines the commands list
+     */
+    public static final HashMap<String, String> COMMANDS = new HashMap<String, String>() {{
+        put("new", "creates a new yuh project");
+        put("generate", "generates a new component");
+    }};
+
     @Override
-    public void run() { }
+    public void run() {
+        System.out.println("Available Commands:");
+
+        COMMANDS.forEach((name, description) -> System.out.println(String.format("  %s %s", name, capitalize(description))));
+    }
 
 }

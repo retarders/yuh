8b2d1a1f173a5c8284447993a8fb1818415dbf73
pcranaway <pcranaway@tuta.io>
2020-11-17 09:45:15 +0000

    add maven template

---
+++ templates/maven/plugin/src/main/java/Plugin.java
@@ -0,0 +1,16 @@
+package {{ project.groupId }}.{{ project.artifactId }}
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onLoad() { }
+
+    @Override
+    public void onEnable() { }
+
+    @Override
+    public void onDisable() { }
+
+}
